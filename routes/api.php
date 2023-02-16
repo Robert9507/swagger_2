3f9cca118798c1b21eb15a02e67ec13a31afedb9
Roberto Chacón <75052626+Robert9507@users.noreply.github.com>
2023-02-16 14:45:57 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,19 @@ use App\Http\Controllers\ApiController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/user', [ApiController::class, 'getUsers']);
+Route::group(["prefix"=>"todo"],function(){
+    Route::get("/get/{id}",[ApiController::class,"get"]);
+    Route::get("/gets",[ApiController::class,"gets"]);
+    Route::post("/store",[ApiController::class,"store"]);
+    Route::put("/update/{id}",[ApiController::class,"update"]);
+    Route::delete("/delete/{id}",[ApiController::class,"delete"]);
+});
